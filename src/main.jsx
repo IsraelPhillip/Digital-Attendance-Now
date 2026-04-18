39dd04ec557ae6d188f60d81d1a615088c12b98e
Israel Phillip <kolybrytme@gmail.com>
2026-04-18 22:51:42 +0100

    import removal in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import App from "./App.jsx";
 import { AuthProvider } from "./lib/authContext.jsx"; // ✅ IMPORT
 // main.jsx
 import "./index.css"; 
-import axios from "axios";
-
-axios.defaults.baseURL = "https://digital-attendance-backend-final.onrender.com";
-axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
